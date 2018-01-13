33bca25aa3c91f798e1c51fe81e35a0d78c153cc
jackS <jastajas@gamil.com>
2018-01-13 17:57:27 +0100

    Nowa klasa Car

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    String brand;
+    String model;
+    int year;
+    double capacity;
+
+    Car(String br, String mo, int yr, double cap) {
+        brand = br;
+        model = mo;
+        year = yr;
+        capacity = cap;
+    }
+}
